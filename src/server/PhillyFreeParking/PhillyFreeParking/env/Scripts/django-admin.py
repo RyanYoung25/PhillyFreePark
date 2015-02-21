1491ee572afa53ceac490c80690d67737dc457b3
Mark Koh <markkohdev@gmail.com>
2015-02-21 11:39:16 -0500

    Committing initial Server project

---
+++ src/server/PhillyFreeParking/PhillyFreeParking/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\cygwin64\home\Mark\PhillyFreePark\src\server\PhillyFreeParking\PhillyFreeParking\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

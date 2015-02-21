b048512ab80e515d3d7babc301a16a0686554f13
Ryan Young <Ryan@rdyoung.us>
2015-02-21 12:27:16 -0500

    Added the model and some key components for the RESTful api

--- src/server/PhillyFreeParking/PhillyFreeParking/app/models.py
+++ src/server/PhillyFreeParking/PhillyFreeParking/app/models.py
@@ -5,3 +5,19 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+
+
+class Parking(models.Model):
+    """This is a model for our parking object"""
+    id = models.IntegerField(primary_key=True)
+    start_lat = models.FloatField(default=0.0)
+    start_long = models.FloatField(default=0.0)
+    end_lat = models.FloatField(default=0.0)
+    end_long = models.FloatField(default=0.0)
+    start_cross = models.CharField(max_length=200)
+    end_cross = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    price = models.FloatField(default=0.0) 
+    verified = models.IntegerField(default=0)
+    date_added = models.DateTimeField('date added')
+    duration = models.FloatField(default=0.0)

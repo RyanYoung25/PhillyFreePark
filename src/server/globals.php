8f7a2d98e55f6225d9c0bedf3f9b66eb1e22afc3
Leo is awesome <lc599@drexel.edu>
2015-02-22 11:05:39 -0500

    accidentally left on local database

--- src/server/globals.php
+++ src/server/globals.php
@@ -4,9 +4,9 @@ $siteContentDB = NULL;
 
 function connectToDB() {
     global $siteContentDB;
-    //$siteContentDB = mysqli_connect('localhost', 'markkoh3_pfp', 'PhillyCodeFest2015', 'markkoh3_phillyfreeparking');
+    $siteContentDB = mysqli_connect('localhost', 'markkoh3_pfp', 'PhillyCodeFest2015', 'markkoh3_phillyfreeparking');
     // Use this for a localserver not on the website
-    $siteContentDB = mysqli_connect('localhost', 'root', '', 'DevPhillyFreePark');
+    //$siteContentDB = mysqli_connect('localhost', 'root', '', 'DevPhillyFreePark');
 }
 
 
